fd8530793e5aacbed888d4ff1d57efb23c21463c
MiriamGaGu <mgg_pjl@yahoo.com>
2019-03-26 22:09:51 -0600

    Changing views

--- src/App.js
+++ src/App.js
@@ -8,43 +8,47 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-      <div style={{height: '300px', position: 'relative'}}>
-    <Layout style={{background: 'url(https://github.com/MiriamGaGuMuk/miriam-portfolio/blob/master/src/assets/activity-adventure-backlit-450062.png?raw=true) center / cover'}}>
-        <Header to='/' transparent title="MiriamGaGu" style={{color: 'white'}}>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Header>
-        <Drawer title="MiriamGaGu">
-            <Navigation>
-              <Link to="/resume">Resume</Link>
-              <Link to="/aboutme">About Me</Link>
-              <Link to="/projects">Projects</Link>
-              <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Drawer>
-        <Content/>
-    </Layout>
-</div>
-<div>
-<Main/>
-</div>
-<footer>
-<Footer size="mini">
-    <FooterSection type="left" logo="Title">
-        <FooterLinkList>
-            <a href="#">Help</a>
-            <a href="#">Privacy & Terms</a>
-        </FooterLinkList>
-    </FooterSection>
-</Footer>
-</footer>
-</React.Fragment>
+        <div>
+          <div clasName="picNav" style={{height: '100px', position: 'relative'}}>
+            <Layout fixedHeader>
+                <Header id="navHeader" to='/' title={<span><span style={{ color: '#ffff' }}></span><strong>MiriamGaGu</strong></span>}>
+                    <Navigation className="menu-links">
+                        <Link to="/resume">Resume</Link>
+                        <Link to="/aboutme">About Me</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/contact">Contact</Link>
+                    </Navigation>
+                </Header>
+                <Drawer title="MiriamGaGu">
+                    <Navigation className="menu-links">
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/aboutme">About Me</Link>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="/contact">Contact</Link>
+                    </Navigation>
+                </Drawer>
+                <Content/>
+              </Layout>
+            </div>
+          <div>
+          <Main/>
+        </div>
+        <footer>
+        <Footer size="mini">
+            <FooterSection type="left" logo="Title">
+                <FooterLinkList>
+                    <Link to="/">Help</Link>
+                    <Link to="/">Privacy & Terms</Link>
+                </FooterLinkList>
+            </FooterSection>
+        </Footer>
+        </footer>
+        </div>
+        </React.Fragment>
     );
   }
 }
 
 export default App;
+
+// style={{background: 'url(https://github.com/MiriamGaGuMuk/miriam-portfolio/blob/master/src/assets/activity-adventure-backlit-450062.png?raw=true) center / cover'}}
--- src/components/projects.js
+++ src/components/projects.js
@@ -3,7 +3,18 @@ import React, {Component} from 'react'
 class Projects extends Component{
     render(){
         return(
-            <div><h1>Projects Page</h1></div>
+            <div className="card">
+                <div className="face face1">
+                    <img src={require('../assets/activity-adventure-backlit-450062.png')}/>
+                </div>
+                <div className="face face2">
+                    <div className="content">
+                        <h2>Hello There</h2>
+                        <p>A little descriptin of the project</p>
+                    </div>
+                </div>
+            
+            </div>
         )
     }
 }
