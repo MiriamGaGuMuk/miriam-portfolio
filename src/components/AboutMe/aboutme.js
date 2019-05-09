3dafce4c9446dd896172f4e94bb4c11d6877702c
MiriamGaGu <mgg_pjl@yahoo.com>
2019-05-08 19:21:10 -0500

    Adding skills section, changing contact, About me better description

--- src/components/AboutMe/aboutme.js
+++ src/components/AboutMe/aboutme.js
@@ -1,7 +1,8 @@
-import React, {Component} from 'react'
-import './aboutme.css'
-import Projects from '../Projects/projects'
-import Contact from '../Contact/contact'
+import React, {Component} from 'react';
+import './aboutme.css';
+import Projects from '../Projects/projects';
+import Contact from '../Contact/contact';
+import {Chip, ChipContact} from 'react-mdl';
 
 class About extends Component{
     render(){
@@ -20,12 +21,10 @@ class About extends Component{
                 </div>
 
                 <section className="section section-light">
-                    <h3 className="title">Y así me convertí en FullStack Developer </h3>
+                    <h3 className="title">¡¡Hey!!
                     <br/>
-                    <p className="aboutBodyTxt">
-                        ¡¡Hey!!
-                        Bienvenid@ a mi página
-                    </p>
+                        Bienvenid@ a mi página </h3>
+                    <br/>
                     <p className="aboutBodyTxt">
                     <strong>¿Quién soy?</strong>
                     <br/>
@@ -36,13 +35,35 @@ class About extends Component{
                     <p className="aboutBodyTxt">
                     <strong>Cambio de carrera</strong>
                     <br/>
-                       De ser un músico a un desarrollador no ha sido fácil y muchos preguntan ¿por qué? ¿cómo fue esa tansición?
+                       De ser un músico a un desarrollador no ha sido fácil y muchos preguntan ¿por qué? ¿cómo fue esa transición?
                        Casi siempre doy la misma respuesta "y... ¿por qué no?
                     </p> 
                     <p className="aboutBodyTxt">
-                    Mucho se habla dentro del mundo tech (al menos en mi experiencia) sobre salirse de la zona de comfort, aprender algo nuevo todos los días, bueno para mi, está fue mi
-                    forma de aprender algo nuevo y salirme de mi zona de comfort
-                    </p>
+                    Mucho se habla dentro del mundo tech (al menos en mi experiencia) sobre salirse de la zona de confort, aprender algo nuevo todos los días, bueno para mí, está fue mi
+                    forma de aprender algo nuevo y salirme de mi zona de confort.
+
+                    Otro punto por el que decidí también aprender a programar es porque todo lo que se pueda digitalizar, se digitalizará, lo que se pueda conectar, se conectará y lo que se pueda analizar, se analizará.
+                    Cuando me decidí a tomar este rumbo me dí cuenta que el software está en todas partes, estamos viviendo una época de automatización inminente.
+                    <br/>
+                    Mi primer pensamiento fue estudiar la carrera de Desarrollo en Software, así lo estoy haciendo, pero hablando con varios que se dedican a esta absorbente y demandante carrera
+                    todos hicieron el mismo comentario "Nos enseñaron a programar hasta lo último de la carrera y no fue mucho" hago un paréntesis aquí, si bien es cierto que no les enseñaron a "programar"
+                    si les enseñaron a tener un pensamiento de abstracción. Me dí entonces a buscar "algo" donde pudiera aprender en poco tiempo o que me diera una pequeña idea de por donde empezar y lo encontré
+                    Bootcamps.
+                    Hay muchas opiniones encontradas respecto a estos programas, en lo personal, no estoy ni completamente a favor ni en contra en su totalidad, considero que todavía es un campo muy nuevo
+                    y que se tendrá que ir explorando. 
+                    <br/>
+                    <br/>
+                    Retomando el hilo, estuve en dos bootcamps, Laboratoria y Muktek aprendí mucho de ambos aunque el primero no lo terminé por cuestiones personales. Es retador todo el tiempo que lo tomas, me puso al límite todo el tiempo
+                    me cuestioné, caí, lloré, me sentí la más tonta del mundo, dudé de mi pero, mentiría si dijera que no aprendí. Estos procesos son muy personales, en mi opinión, demandan mucho mentalmente
+                    y no todos y me incluyo, somos capaces de sobrellevar el proceso pero el éxito en cada uno de los pequeños o grandes proyectos es indefinible.
+                    <br/>
+                    <br/>
+                    De lo mejor que puedes tener es estos procesos son los amigos, conocidos y contactos, las experiencias que adquieres en el entorno personal son igual (me atrevo a decir más) de satisfactorias que las técnicas. En mi generación de Muktek
+                    "LAMAT" la complicidad que hubo todo el tiempo, la amistad que se generó y las experiencias vividas fueron únicas y enriquecedoras (perdón a nuestro instructor por los malos ratos que le hicimos pasar de se así).
+                 </p>
+                 <p className="aboutBodyTxt">
+                    No tengo idea de a donde me llevará este nuevo camino, pero si voy a cometer errores que sean nuevos, además he aprendido en este proceso que los errores son el mejor maestro. Adoro los misterios, hay partes que parecen no encajar, pero al final lo hacen y todo cobra sentido.
+                 </p>
                 </section>
 
                 <div className="pimg2">
@@ -55,7 +76,6 @@ class About extends Component{
                 </div>
                 
                 <section className="section section-dark">
-                    {/* <h2>Projects</h2> */}
                     <p>
                     <Projects/>
                     </p>
@@ -63,17 +83,86 @@ class About extends Component{
 
                 <div className="pimg3">
                     <div className="ptext">
-                    <span className="border trans">
-                        Image Three Text
+                    <span className="border">
+                        Habilidades
                     </span>
                     </div>
                 </div>
 
                 <section className="section section-dark">
-                    <h2>Section Three</h2>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi, fugit amet tempora facere dolores nisi facilis consequatur, odio hic minima nostrum. Perferendis eos earum praesentium, blanditiis sapiente labore aliquam ipsa architecto vitae. Minima soluta temporibus voluptates inventore commodi cumque esse suscipit optio aliquam et, dolorem a cupiditate nihil fuga laboriosam fugiat placeat dignissimos! Unde eveniet placeat quisquam blanditiis voluptatem doloremque fugiat dolor repellendus ratione in.
-                    </p>
+                <div>
+                    <h4>Desarrollo</h4>
+                <Chip>
+                <ChipContact style={{ background: 'url("http://www.iconninja.com/files/541/586/346/command-language-software-develop-code-programming-javascript-icon.png") 0 0 / cover' }}/>
+                    JavaScript
+                </Chip>
+                </div>
+                <div>
+                <Chip>
+                <ChipContact style={{ background: 'url("https://video-react.js.org/assets/logo.png") 0 0 / cover' }}/>
+                    ReactJS
+                </Chip>
+                </div>
+                <div>
+                <Chip>
+                <ChipContact style={{ background: 'url("https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/256/square_256/nodejslogo.png") 0 0 / cover' }}/>
+                    NodeJs
+                </Chip>
+                </div>
+                <div>
+                <Chip>
+                <ChipContact style={{ background: 'url("https://www.weheartswift.com/wp-content/uploads/2017/05/Firebase_16-logo.png") 0 0 / cover' }}/>
+                    Firebase
+                </Chip>
+                </div>
+                <div>
+                <Chip>
+                <ChipContact style={{ background: 'url("https://3.bp.blogspot.com/-NLgOIL0vKJg/W0yrvqx8ZsI/AAAAAAAABfg/l7fFkNQf5S02YIanj4H9KKWiZJNmSlKigCLcBGAs/s1600/mongodb.png") 0 0 / cover' }}/>
+                    MongoDB
+                </Chip>
+                </div>
+                <div>
+                <Chip>
+                <ChipContact style={{ background: 'url("https://gurayyarar.gallerycdn.vsassets.io/extensions/gurayyarar/expressjs-4-snippets/1.0.2/1512033439660/Microsoft.VisualStudio.Services.Icons.Default") 0 0 / cover' }}/>
+                    Express
+                </Chip>
+                </div>
+                <div>
+                <Chip>
+                <ChipContact style={{ background: 'url("https://d2.alternativeto.net/dist/icons/koa_37212.png?width=128&height=128&mode=crop&upscale=false") 0 0 / cover' }}/>
+                    Koa
+                </Chip>
+                </div>
+                <div>
+                <Chip>
+                <ChipContact style={{ background: 'url("https://justcodeit.io/wp-content/uploads/Git_icon.svg_.png") 0 0 / cover' }}/>
+                    Git
+                </Chip>
+                </div>
+                <div>
+                <Chip>
+                <ChipContact style={{ background: 'url("https://camo.githubusercontent.com/7710b43d0476b6f6d4b4b2865e35c108f69991f3/68747470733a2f2f7777772e69636f6e66696e6465722e636f6d2f646174612f69636f6e732f6f637469636f6e732f313032342f6d61726b2d6769746875622d3235362e706e67") 0 0 / cover' }}/>
+                    Github
+                </Chip>
+                </div>
+                <div>
+                <Chip>
+                <ChipContact style={{ background: 'url("http://cdn.slidesharecdn.com/profile-photo-GitLab-96x96.jpg?cb=1523568873") 0 0 / cover' }}/>
+                    Gitlab
+                </Chip>
+                </div>
+                <div>
+                <Chip>
+                <ChipContact style={{ background: 'url("http://dpd7q0cxjbvq3.cloudfront.net/images/autotweet-documentation/autotweet-documentationhow-to-own-facebook-heroku-app/heroku.png") 0 0 / cover' }}/>
+                    Heroku
+                </Chip>
+                </div>
+                <div>
+                <Chip>
+                <ChipContact style={{ background: 'url("https://d2.alternativeto.net/dist/icons/netlify-cms_89329.png?width=200&height=200&mode=crop&upscale=false") 0 0 / cover' }}/>
+                    Netlify
+                </Chip>
+                </div>
                 </section>
 
                 <div className="pimg4">
@@ -85,7 +174,7 @@ class About extends Component{
                 </div>
 
                 <section className="section section-dark">
-                    <h2>Contact</h2>
+                    <h2 id="contacto">Contacto</h2>
                    <Contact/>
                 </section>
             </React.Fragment>
--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -9,7 +9,7 @@ class Contact extends Component{
             <div>
             <content >
                 <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
-                    <CardTitle  style={{color: '#fff', height: '150px', background: 'url(http://www.lifeinsideuk.com/wp-content/uploads/2018/10/Contact-Us-514727914-1920x890-Oct-20.jpg) center / cover'}}></CardTitle>
+                    <CardTitle  style={{color: '#fff', height: '350px', background: 'url(https://media1.giphy.com/media/zSiEygkcONzyw/giphy.gif?cid=790b76115cd3683a335a2e6c515684b3&rid=giphy.gif) center / cover'}}></CardTitle>
                     <CardText>
                     <List>
                       <ListItem>
